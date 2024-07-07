934a5272be350e760cae7daa4ae25704ead89b3e
sailorspace <sanjaymahara@gmail.com>
2024-07-07 17:48:11 +0530

    handle cookie and check authentication from cookie value

--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -9,10 +9,21 @@ Path=/; Expires=Mon, 07 Jul 2025 11:00:07 GMT; */
 
 router.get("/api/products", (req, res) => {
 
-    console.log(`Products Get Called: ${req.url}`)
+    console.log(`1.Products Get Called: ${req.url}`)
     //console.log(req.header.signedCookies);
     //suppose we get these info from the databse based on the used id in the claim
-    const userInfo = { "username": "sanjaymahara", "password": "password12" }; 
+    const cookieData = req.cookies.userinfo; //get the cookie as recieved from the client
+    const username = cookieData.toString().split(0, cookieData.indexOf('-'))
+    console.log(`2.cookiedata ${cookieData}`);
+    console.log(`3.username is : ${username}`);
+    //mock call to db ti get userinfo based on username 
+    const dummyDBCall = (username) => {
+        //passing username argument to some db proc should return data
+        return { "username": "sanjaymahara", "password": "733a450e15ead43c04df07deae32720133ac5e6824eb34932d2e57223776391e" };
+    };
+    
+    const userInfo = dummyDBCall(username); //call the getuserform db function
+    console.log(`4 db user ${JSON.stringify(userInfo)}`);
     //in real world password will be a hashed value
     const hash = crypto.createHash('sha256');
     hash.update(userInfo.toString());//feed the data to the hashfunction
@@ -22,10 +33,8 @@ router.get("/api/products", (req, res) => {
     //now suppose we use the userinfo(username,password) from the session ,
     //and compare this with the cookie we can verify the call
     const salt = userInfo.username + "-"; //create a salt or secret value
-    const getCookieToSet = salt+digest;
-    const cookieData = req.cookies.userinfo; //get the cookie as recieved from the client
-    console.log(cookieData);
-    console.log(getCookieToSet);
+    const getCookieToSet = salt + digest;
+    console.log(`5. setting the cookie for browser ${getCookieToSet}`);
     if (cookieData != getCookieToSet) return res.status(401).send("Unauthorised Access");
     res.cookie("userinfo", getCookieToSet, { maxAge: 6000 });//if all good reset the cookie
     res.status(200).send(
