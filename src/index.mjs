836af938f5288ab055fcdcac751a2cf00c741464
sailorspace <sanjaymahara@gmail.com>
2024-07-08 00:47:47 +0530

    add session validation

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,7 @@ import express, { response } from 'express';
 import routes from "./routes/routes.mjs";
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import { mockUsers } from './utils/constants.mjs';
 /* import userRouter from "../routes/user.mjs";
 import productsRouter from "../routes/products.mjs"; */
 
@@ -53,4 +54,35 @@ app.get("/", (req, res) => {
     req.session.visited = true; //this prevents creating new sessionid for every request by client
     res.cookie("hello", "world", { maxAge: 30000, signed: true });
     res.status(201).send({ msg: "Hello There" });
+});
+
+app.post("/api/auth", (req, res) => {
+    const { username, password } = req.body;
+    const findUser = mockUsers.find((user) => user.name === username);
+    if (!findUser || (findUser.password != password))
+        return res.status(401).send({ msg: "Bad Credentials" });
+    //the cookie sent to the client side holds only the session id not the session value
+    req.session.user = findUser; //attatch dynamic property to nodejs objects 
+    //the above creates a cookie which store the sessionid 
+    return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+    console.log(req.session.user);
+    req.sessionStore.get(req.sessionID, (error, session) => {
+        console.log(session);
+    });
+    //for any api endpoint the way to authenticate the user is to check the session for
+    //the request, every browser agent will have a different session id created
+    //event if the authentication is for the same user , from different browser agent we will have
+    //two different sessions created
+    return req.session.user ? res.status(200).send(req.session.user)
+        : res.status(401).send({ msg: "Not Authenticated" })
+});
+
+app.post("/api/updateuser", (req, res) => {
+    return req.session.user ? res.send({ msf: "user updated" })
+        : res.status(401).send({ msg: "Not Authenticated" });
+    //for user update we can check if the sent user to update is the same as the 
+    //one in the session store by matching the id or some hash value
 });
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,10 +1,10 @@
 export const mockUsers = [
-    { id: 1, name: 'sanjay', city: "noida" },
-    { id: 2, name: 'jack', city: "new york" },
-    { id: 3, name: 'mouan', city: "beijing" },
-    { id: 4, name: 'lee', city: "tokyo" },
-    { id: 5, name: 'kobondo', city: "cairo" },
-    { id: 6, name: 'jackie', city: "veinna" },
-    { id: 7, name: 'ellei', city: "denmark" }
+    { id: 1, name: 'sanjay', city: "noida", password: "1234" },
+    { id: 2, name: 'jack', city: "new york", password: "12345" },
+    { id: 3, name: 'mouan', city: "beijing", password: "123456" },
+    { id: 4, name: 'lee', city: "tokyo", password: "1234567" },
+    { id: 5, name: 'kobondo', city: "cairo", password: "12345678" },
+    { id: 6, name: 'jackie', city: "veinna", password: "123456789" },
+    { id: 7, name: 'ellei', city: "denmark", password: "1234567890" },
 ];
 
